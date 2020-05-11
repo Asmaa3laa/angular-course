47ab0bb1c08bc42bd7245ecc7acfc3c09c6e2f94
Asmaa Alaa <asmaa.3laa1996@gmail.com>
2020-05-11 17:50:28 +0200

    angularLabs

---
+++ src/app/_service/department.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Department } from '../_models/department';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentService {
+  private depts:Department[]=[
+    new Department(1,"OS"),
+    new Department(2,"IOT"),
+  ];
+  getAll(){
+    return this.depts;
+  }
+  addDepartment(dept:Department){
+    this.depts.push(new Department(dept.id,dept.deptName));
+  }
+  getDepartment(id:number):Department{
+    const index = this.depts.findIndex(d => d.id === id);
+    if (index != -1){
+      return this.depts[index];
+    }
+    return null;
+  }
+  deleteDepartment(id:number){
+    this.depts.splice(id, 1);
+  }
+  editDepartment(dept: Department){
+    const index = this.depts.findIndex(s => s.id === dept.id);
+    this.depts[index]=new Department(dept.id,dept.deptName);
+  }
+  constructor() { }
+}
---
+++ src/app/_service/student.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Student } from '../_models/student';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+  private students:Student[]=[
+    new Student(1,"Asmaa",23),
+    new Student(2,"Ahmed",21),
+  ];
+  getAll(){
+    return this.students;
+  }
+  addStudent(std:Student){
+    this.students.push(new Student(std.id,std.name,std.age));
+  }
+  getStudent(id:number):Student{
+    const index = this.students.findIndex(s => s.id === id);
+    if (index != -1){
+      return this.students[index];
+    }
+    return null;
+  }
+  deleteStudent(id:number){
+    this.students.splice(id, 1);
+  }
+  editStudent(std: Student){
+    const index = this.students.findIndex(s => s.id === std.id);
+    this.students[index]=new Student(std.id,std.name,std.age);
+  }
+  constructor() { }
+}
